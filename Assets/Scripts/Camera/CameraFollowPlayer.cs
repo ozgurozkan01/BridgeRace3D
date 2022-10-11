0b019383ae536b75c3dbbfd48cab57dbd38e5ae5
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2022-10-11 21:30:48 +0300

    initial commit

---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] private float lerpMultiplier;
+    private Vector3 _offset;
+    private GameObject _player;
+    
+    void Start()
+    {
+        _player = GetComponent<GameObject>();
+        _player = GameObject.Find("Player");
+
+        _offset = transform.position - _player.transform.position;
+    }
+    
+    void Update()
+    {
+        CameraMove();
+    }
+
+    private void CameraMove()
+    {
+        transform.position = Vector3.Lerp(transform.position, _player.transform.position + _offset, lerpMultiplier * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Camera _cam;
+    private Collider _planeCollider;
+    private RaycastHit _raycastHit;
+    private Ray _ray;
+    
+    private void Start()
+    {
+        _cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _planeCollider = GameObject.Find("Plane").GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        GetMousePos();
+        
+        if (Input.GetMouseButton(0))
+        {
+            PlayerFollowTheMouse();    
+        }
+    }
+
+    private void GetMousePos()
+    {
+        _ray = _cam.ScreenPointToRay(Input.mousePosition);
+    }
+
+    private void PlayerFollowTheMouse()
+    {
+        if (Physics.Raycast(_ray, out _raycastHit))
+        {
+            if (_raycastHit.collider == _planeCollider)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, _raycastHit.point, Time.deltaTime * speed);
+                transform.LookAt(new Vector3(_raycastHit.point.x, transform.position.y, _raycastHit.point.z));
+            }
+        }
+    }
+}
